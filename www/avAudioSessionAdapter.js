79bbbf74494094b362e583c7f82119d3adb629aa
eworx <dev@eworx.gr>
2014-06-05 15:25:59 +0300

    Fixed cordova.exec calls

--- www/avAudioSessionAdapter.js
+++ www/avAudioSessionAdapter.js
@@ -5,7 +5,7 @@ var AVAudioSessionAdapter = function() {
 };
 
 AVAudioSessionAdapter.prototype.getCategory = function(resultCallback) {
-    return cordova.exec(successCallback, errorCallback, "AVAudioSessionAdapter", "getCategory", []);
+    exec(successCallback, errorCallback, "AVAudioSessionAdapter", "getCategory", []);
         
     function successCallback(param) {
         resultCallback(param);
@@ -15,7 +15,7 @@ AVAudioSessionAdapter.prototype.getCategory = function(resultCallback) {
 };
 
 AVAudioSessionAdapter.prototype.getCategoryOptions = function(resultCallback) {
-    return cordova.exec(successCallback, errorCallback, "AVAudioSessionAdapter", "getCategoryOptions", []);
+    exec(successCallback, errorCallback, "AVAudioSessionAdapter", "getCategoryOptions", []);
         
     function successCallback(param) {
             resultCallback(param);
@@ -25,11 +25,11 @@ AVAudioSessionAdapter.prototype.getCategoryOptions = function(resultCallback) {
 };
 
 AVAudioSessionAdapter.prototype.setCategory = function(successCallback, errorCallback, category) {
-    return cordova.exec(successCallback, errorCallback, "AVAudioSessionAdapter", "setCategory", [category]);
+    exec(successCallback, errorCallback, "AVAudioSessionAdapter", "setCategory", [category]);
 };
 
 AVAudioSessionAdapter.prototype.setCategoryWithOptions = function(successCallback, errorCallback, category, categoryOptions) {
-    return cordova.exec(successCallback, errorCallback, "AVAudioSessionAdapter", "setCategory", [category, categoryOptions]);
+    exec(successCallback, errorCallback, "AVAudioSessionAdapter", "setCategory", [category, categoryOptions]);
 };
 
 module.exports = AVAudioSessionAdapter;
