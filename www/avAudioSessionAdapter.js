b341c5e4472546bfb345589c624251bab334ce6b
eworx <dev@eworx.gr>
2014-06-05 17:13:54 +0300

    Audio session category and category options constants

--- www/avAudioSessionAdapter.js
+++ www/avAudioSessionAdapter.js
@@ -4,6 +4,23 @@ var exec = require('cordova/exec');
 var AVAudioSessionAdapter = function() {
 };
 
+AVAudioSessionAdapter.audioSessionCategories = {
+    AMBIENT = 'AVAudioSessionCategoryAmbient';
+    SOLO_AMBIENT = 'AVAudioSessionCategorySoloAmbient',
+    PLAYBACK = 'AVAudioSessionCategoryPlayback',
+    RECORD = 'AVAudioSessionCategoryRecord',
+    PLAY_AND_RECORD = 'AVAudioSessionCategoryPlayAndRecord',
+    AUDIO_PROCESSING = 'AVAudioSessionCategoryAudioProcessing',
+    MULTI_ROUTE = 'AVAudioSessionCategoryMultiRoute'
+};
+
+AVAudioSessionAdapter.audioSessionCategoryOptions = {
+    MIX_WITH_OTHERS = 1,
+    DUCK_OTHERS = 2,
+    ALLOW_BLUETOOTH = 4,
+    DEFAULT_TO_SPEAKER = 8
+}
+
 AVAudioSessionAdapter.prototype.getCategory = function(resultCallback) {
     exec(successCallback, errorCallback, "AVAudioSessionAdapter", "getCategory", []);
         
